a2bdc368adf3aaa315bb761351be1018c64f7e0c
luisgepeto <luisgepeto@gmail.com>
2016-10-24 12:01:12 -0500

    added readme and intro to lists

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HaskellLearning
 Haskell examples from Learn You a Haskell for Great Good!
+
+http://learnyouahaskell.com/
+
+In order to execute the following samples the Haskell platform needs to be installed.. 
+In order to execute the code from the command line you need to run the following:
+
+    ghci
+    :l myfunctions.hs -- to load the module
+    :r -- reloads all modules
---
+++ Starting Out/intro_to_lists.hs
@@ -0,0 +1,25 @@
+lostNumbers = [4, 8, 15, 16, 23, 42]
+concatenatedLists = [1,2,3,4] ++ [9,10,11,12]  
+concatenatedStrings = "hello" ++ " " ++ "world"  
+concatenatedListOfChars = ['w','o'] ++ ['o','t']  
+smallCat = 'A': "SMALL CAT"
+insertOperator = 5:[1, 2, 3, 4, 5]
+indexOperator = "Steve Buscemi" !! 6
+indexOperator' = "Steve Buscemi" !! 20
+b = [[1,2,3,4],[5,3,3,3],[1,2,2,3,4],[1,2,3]]  
+c = b ++ [[1,1,1,1]]
+d = [6,6,6]:b  
+e = b !! 2  
+
+
+
+--Comparisons
+greater = [3,2,1] > [2,1,0]  
+
+--List Operations
+head' = head [5, 4, 3, 2, 1]
+tail' = tail [5, 4, 3, 2, 1]
+last' = last [5,4,3,2,1]  
+init' = init [5,4,3,2,1]  
+length' = length [5,4,3,2,1]
+null' = null[]
