83f62d92072a8016f79cffd5ccba3c10588adc5b
luisgepeto <luisgepeto@gmail.com>
2016-10-31 17:04:19 -0600

    added cases

---
+++ Syntax In Functions/case.hs
@@ -0,0 +1,21 @@
+-- pattern matchin on parameters in functions definition
+head' :: [a] -> a  
+head' [] = error "No head for empty lists!"  
+head' (x:_) = x  
+
+head'' :: [a] -> a  
+head'' xs = case xs of  [] -> error "No head for empty lists!"  
+                        (x:_) -> x  
+
+-- cases can be used anywhere not only on function definitions
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list."  
+
+
+describeList' :: [a] -> String  
+describeList' xs = "The list is " ++ what xs  
+    where what [] = "empty."  
+          what [x] = "a singleton list."  
+          what xs = "a longer list."  
