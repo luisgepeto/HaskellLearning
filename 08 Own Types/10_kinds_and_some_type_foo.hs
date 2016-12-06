e1b3c5ac0cb7e35fc88e86a73dde1f5a4df62bbe
luisgepeto <luisgepeto@gmail.com>
2016-12-06 13:08:49 -0600

    added kinds and io folder

---
+++ 08 Own Types/10_kinds_and_some_type_foo.hs
@@ -0,0 +1,20 @@
+--types can also be partially applied
+--types have their own metatype called kind
+-- :k Int   -> this returns Int :: *
+--A star means that it is a concrete types
+
+-- :k Maybe
+-- Maybe :: * -> *
+--Maybe means that it takes a concrete type and returns a concrete type
+
+-- :k Maybe Int
+-- Maybe Int :: *
+
+-- :k Either  
+-- Either :: * -> * -> *
+--we canpartially apply Either
+-- :k Either String  
+-- Either String :: * -> *  
+--further applying
+-- :k Either String Int  
+-- Either String Int :: *  
